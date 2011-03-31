0ed548a03212dc17d7914db9fba8b598a9e21736
Remo Giermann <mo@liberejo.de>
2011-03-31 02:56:33 +0200

    using a simple function instead of class

--- latestxspf/latestxspf.py
+++ latestxspf/latestxspf.py
@@ -20,11 +20,18 @@ findstring = 'find %s -mtime -%i -and \( -iname "*.mp3" -or -iname "*.ogg" -or -
 
 
 def findfiles(directory, mtime):
+	"""
+	Finds audio files in 'directory' modified in the last 'mtime' days.
+	"""
 	listing = commands.getoutput(findstring % (directory, mtime))
 	return listing.split('\n')
 
 
 def tag2dict(filename):
+	"""
+	Reads tag info from 'filename' and returns a directory with artist, title 
+	and album strings.
+	"""
 	tag = tagpy.FileRef(filename).tag()
 	d = {}
 	d.update("artist", tag.artist or "Unknown Artist")
@@ -34,25 +41,18 @@ def tag2dict(filename):
 	return d
 
 
-class Tracklist(object):
+def latesttracks(directory, days):
 	"""
-	Holds a list of music files, generates a list of tracks and may
-	generate an XSPF playlist.
+	Finds the latest additions to 'directory' (within the last 'days')
+	and returns an XSPF playlist.
 	"""
 
-	def __init__(self, files):
-		self.files  = files
-		self.tracks = []
+	files  = findfiles(directory, days)
+	tracks = [tag2dict(f) for f in self.files]
 	
-	def parseFiles(self):
-		self.tracks = [tag2dict(f) for f in self.files]
-	
-	def genXSPF(self):
-		xspf = xspfgenerator.SimpleXSPFGenerator()
-		for t in self.tracks:
-			xspf.addTrack(t)
-		
-		return str(xspf)
+	xspf = xspfgenerator.SimpleXSPFGenerator()
+	xspf.addTracks(self.tracks)
+	return str(xspf)
 
 
 
@@ -60,9 +60,6 @@ if __name__ == "__main__":
 	import sys
 	argv, argc = sys.argv, len(sys.argv)
 	if argc < 3:
-		print "Usage: %s DIRECTORY DAYS" % argv
+		print "Usage: %s DIRECTORY DAYS" % argv[0]
 	else:
-		files = findfiles(argv[1], argv[2])
-		t = Tracklist(files)
-		t.parseFiles()
-		print t.genXSPF()
+		print latesttracks(argv[1], argv[2])
