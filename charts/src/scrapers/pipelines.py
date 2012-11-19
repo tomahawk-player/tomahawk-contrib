84b68ad23303ce301b606ee4fbd07b5c81c5c487
Hugo Lindström <hugolm84@gmail.com>
2012-11-19 12:17:49 +0100

    Move Pipeline, utilize chartCache storage

--- charts/src/scrapers/pipelines.py
+++ charts/src/scrapers/pipelines.py
@@ -2,11 +2,10 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/topics/item-pipeline.html
-from time import gmtime, strftime
-import shove
-from scrapy.conf import settings
 from scrapy.exceptions import DropItem
 from scrapy import log
+
+from sources.utils import cache as chartCache
 
 class ScrapersPipeline(object):
 
@@ -14,8 +13,8 @@ class ScrapersPipeline(object):
         self.storage = None
 
     def open_spider(self, spider):
-        self.storage = shove.Shove("file://"+settings['OUTPUT_DIR']+'/sources', optimize=False)
-
+        self.storage = chartCache.storage
+        return
 
     def process_item(self, item, spider):
 
@@ -26,17 +25,25 @@ class ScrapersPipeline(object):
             raise DropItem("Missing source: %s" % item)
 
         source = item['source']
+        # This is a little hack, for now, we only get new releases from Itunes
+        # and its identified by extra field
+        try :
+            if source == "itunes" and item['extra'] :
+                self.storage = chartCache.newreleases
+            else : self.storage = chartCache.storage
+        except :
+            self.storage = chartCache.storage
+        
         chart_id = source+item['id']
         log.msg("Saving %s - %s" %(source, item['id']))
 
-        list = self.storage.get(source, {})
+        chart_list = self.storage.get(source, {})
 
         # metadata is the chart item minus the actual list plus a size
         metadata_keys = filter(lambda k: k != 'list', item.keys())
         metadata = { key: item[key] for key in metadata_keys }
         metadata['size'] = len(item['list'])
-        metadata['date'] = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
-        list[chart_id] = metadata
-        self.storage[source] = list
+        chart_list[chart_id] = metadata
+        self.storage[source] = chart_list
         self.storage[chart_id] = dict(item)
         return item
