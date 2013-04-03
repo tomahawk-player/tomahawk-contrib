3fa4a6546ec2830238031368521c94c6ac3ec0c0
Hugo Lindström <hugolm84@gmail.com>
2013-04-03 10:24:26 +0200

    Fix for DjShop.de expiration

--- charts/src/scrapers/spiders/djshop_spider.py
+++ charts/src/scrapers/spiders/djshop_spider.py
@@ -25,68 +25,62 @@ from scrapy.selector import HtmlXPathSelector
 from scrapy.contrib.loader import XPathItemLoader
 from scrapy.http import Request
 from scrapy import log
-from scrapers.items import ChartItem, SingleTrackItem, SingleAlbumItem, SingleArtistItem, slugify
-from collections import deque
+from scrapers.items import ChartItem, SingleAlbumItem, slugify
 from sources.utils import cache as chartCache
 import re
 import urllib2
 
-@chartCache.methodcache.cache('get_chart_url', expire=settings['ITUNES_EXPIRE'])
-def get_chart_urls():
-    chart_list = []
-    for chart in ["http://www.djshop.de/Download-Charts/ex/s~mp3,u~charts/xe/Download-Charts.html", "http://www.djshop.de/Vinyl-Charts/ex/s~charts/xe/charts.html"]:
-        print "parsing " + chart
-        req = urllib2.Request(chart)
-        hxs = HtmlXPathSelector(text=urllib2.urlopen(req).read())
-        try:
-            navBox = hxs.select('//div[@id="leftColumn"]')
-            navList = navBox.select('//ul[@class="navUL"]/li')
-            for index, link in enumerate(navList):
-                chart_name = link.select('a/text()').extract()[0].strip()
-                if not "Label Charts" in chart_name:
-                    chart_list.append("http://www.djshop.de" + link.select('a/@href').extract()[0].strip())
-        except Exception, e:
-            print e
-    return chart_list
 
 class DjShopSpider(CrawlSpider):
     name = "djshop.de"
     allowed_domains = ["djshop.de"]
-    start_urls = get_chart_urls()
-    '''
-        Expires tomorrow
-    '''
-    def get_maxAge(self) :
-        today = datetime.utcnow()
-        expires = datetime.replace(today +  timedelta(days=1),hour=1, minute=0, second=0)
-        maxage = expires-today
-        return maxage.seconds
+    baseUrl = "http://www.djshop.de/"
+    baseCharts = [ "%sDownload-Charts/ex/s~mp3,u~charts/xe/Download-Charts.html" % baseUrl,
+                   "%sVinyl-Charts/ex/s~charts/xe/charts.html" % baseUrl ]
+
+    chartTypes = [{"unpretty" : "MP3 Downloads Charts", "pretty" : "Digital Charts"}, \
+                  {"unpretty" : "Charts Style Charts", "pretty" : "Vinyl Charts"}, \
+                  {"unpretty" : "Charts Top 100", "pretty" : "Top 100"}, \
+                  {"unpretty" : "Charts International Charts", "pretty" : "International Charts"}]
+                                 
+    def __init__(self, name=None, **kwargs):
+        super(DjShopSpider, self).__init__()
+        self.get_chart_urls()
+
+    def get_chart_urls(self):
+        for chart in self.baseCharts:
+            req = urllib2.Request(chart)
+            hxs = HtmlXPathSelector(text=urllib2.urlopen(req).read())
+            try:
+                navBox = hxs.select('//div[@id="leftColumn"]')
+                navList = navBox.select('//ul[@class="navUL"]/li')
+                for index, link in enumerate(navList):
+                    if not "Label Charts" in link.select('a/text()').extract()[0].strip():
+                        self.start_urls.append("http://www.djshop.de" + link.select('a/@href').extract()[0].strip())
+            except Exception, e:
+                print e
+
+    
     
     def parse(self, response):
         log.msg("Parsing: %s"%(response.url), loglevel=log.INFO)
         hxs = HtmlXPathSelector(response)
-
         chart = ChartItem()
-
         title = hxs.select("//title/text()").extract()[0].strip()
-        types = [ {"unpretty" : "MP3 Downloads Charts", "pretty" : "Digital Charts"}, \
-                  {"unpretty" : "Charts Style Charts", "pretty" : "Vinyl Charts"}, \
-                  {"unpretty" : "Charts Top 100", "pretty" : "Top 100"}, \
-                  {"unpretty" : "Charts International Charts", "pretty" : "International Charts"}]
-
         test = re.compile('^(MP3 Downloads(\sCharts|\s))(.*?)(\sCharts)', re.IGNORECASE)
+        
         try:
             cTitle = test.match(title).group(3)
             if cTitle is not None:
-                type = types[2]["pretty"]+" ";
+                type = self.chartTypes[2]["pretty"]+" ";
                 if "vinyl" in response.url.lower() :
-                    type += types[1]["pretty"]
+                    type += self.chartTypes[1]["pretty"]
                 else :
-                    type += types[0]["pretty"]
+                    type += self.chartTypes[0]["pretty"]
                 chart["extra"] = type;
-                chart["name"] = cTitle.replace(types[2]["pretty"], "")
+                chart["name"] = cTitle.replace(self.chartTypes[2]["pretty"], "")
         except Exception:
-            for type in types:
+            for type in self.chartTypes:
                 if type["unpretty"] in title :
                     chart["extra"] = type["pretty"]
                     cTitle = title.replace(type["unpretty"], "")
@@ -97,9 +91,9 @@ class DjShopSpider(CrawlSpider):
                     if "Top 100" in chart["extra"] :
                         chart["extra"] += " "
                         if "vinyl" in response.url.lower() :
-                            chart["extra"] += types[1]["pretty"]
+                            chart["extra"] += self.chartTypes[1]["pretty"]
                         else :
-                            chart["extra"] += types[0]["pretty"]
+                            chart["extra"] += self.chartTypes[0]["pretty"]
                         chart["name"] = chart["name"].replace("Charts", "")
 
         if "name" in chart :
@@ -108,11 +102,12 @@ class DjShopSpider(CrawlSpider):
             chart['source'] = 'djshop.de'
             chart['id'] = slugify(chart["extra"] + chart["name"])
             chart["type"] = "Album"
-            chart['list'] = []
-            cacheControl = chartCache.setCacheControl(self.get_maxAge())
+            cacheControl = chartCache.setCacheControl(chartCache.getMaxAge())
             chart['date'] = cacheControl.get("Date-Modified")
             chart['expires'] = cacheControl.get("Date-Expires")
             chart['maxage'] = cacheControl.get("Max-Age")
+
+            chart['list'] = []
             
             '''
                 This could be transformed into a track chart
