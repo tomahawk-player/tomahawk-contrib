b34b758713b1a9d6c32d40ba0d50f1898a476208
Hugo Lindström <hugolm84@gmail.com>
2013-04-05 01:34:21 +0200

    Display_name

--- charts/src/scrapers/apis/soundcloudwall.py
+++ charts/src/scrapers/apis/soundcloudwall.py
@@ -73,7 +73,7 @@ def parseUrl(url, title, default):
         chart_type = _type
         chart = ChartItem()
         chart['name'] = chart_name
-        chart['display_name'] = chart["name"] if chart["name"] else "Top Overall"
+        chart['display_name'] = chart_name if chart_name else "Top Overall"
         chart['source'] = source
         chart['type'] = chart_type
         chart['default'] = default
